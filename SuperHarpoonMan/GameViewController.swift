81b3a7ee882bb8af557777490e070090b7bb5bd6
Steve Palley <spall03@gmail.com>
2015-09-30 10:05:45 -0700

    updated for Swift 2

--- SuperHarpoonMan/GameViewController.swift
+++ SuperHarpoonMan/GameViewController.swift
@@ -13,12 +13,18 @@ let superHarpoonManGameIsOver = "super_harpoon_man_game_is_over"
 
 extension SKNode {
     class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+        if let path = NSBundle.mainBundle().pathForResource(file as String, ofType: "sks") {
+            
+            var sceneData: NSData?
+            do {
+                sceneData = try  NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
+            } catch _ as NSError {
+                
+            }
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData!)
             
             archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
             archiver.finishDecoding()
             return scene
         } else {
@@ -39,7 +45,7 @@ class GameViewController: UIViewController {
 
         gameScene = GameScene.unarchiveFromFile("GameScene") as? GameScene
             // Configure the view.
-            gameView = self.view as SKView
+            gameView = self.view as! SKView
             gameView.showsFPS = true
             gameView.showsNodeCount = true
             
@@ -101,11 +107,11 @@ class GameViewController: UIViewController {
         return true
     }
 
-    override func supportedInterfaceOrientations() -> Int {
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+            return UIInterfaceOrientationMask.AllButUpsideDown
         } else {
-            return Int(UIInterfaceOrientationMask.All.rawValue)
+            return UIInterfaceOrientationMask.All
         }
     }
 
